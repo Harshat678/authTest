4519e32ed7d8847b0004d410daa62e360cd1c3b2
harshat b <101925630+Harshat678@users.noreply.github.com>
2024-03-06 19:30:37 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dbURI = 'mongodb+srv://harshatborusu518:harshat123@cluster0.emekczo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const dbURI = 'mongodb+srv://<username>:<password>@cluster0.emekczo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(dbURI)
   .then((result) => console.log('Connected to DB'))
